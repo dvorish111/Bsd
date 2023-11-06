e0a3922bb12a0cf4c26076ba775389d1a2932b43
chana <ch0556766286b@gmail.com>
2023-11-06 13:09:46 +0200

    Add Repository CURD in progres

--- DAL/Repository/CampaignRepository.cs
+++ DAL/Repository/CampaignRepository.cs
@@ -1,12 +1,55 @@
-﻿using System;
+﻿using DAL.Models;
 using System.Collections.Generic;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace DAL.Repository
 {
-    internal class CampaignRepository
+    public class CampaignRepository
     {
+        private readonly CampainContext _context;
+
+        public CampaignRepository(CampainContext context)
+        {
+            _context = context;
+        }
+
+        public List<Campaign> GetAllCampaigns()
+        {
+            return _context.Campaigns.ToList();
+        }
+
+        public Campaign GetCampaignById(int campaignId)
+        {
+            return _context.Campaigns.FirstOrDefault(c => c.Id == campaignId);
+        }
+
+        public void AddCampaign(Campaign campaign)
+        {
+            _context.Campaigns.Add(campaign);
+            _context.SaveChanges();
+        }
+
+        public void UpdateCampaign(Campaign campaign)
+        {
+            var existingCampaign = _context.Campaigns.FirstOrDefault(c => c.Id == campaign.Id);
+            if (existingCampaign != null)
+            {
+                existingCampaign.Name = campaign.Name;
+                existingCampaign.StartDate = campaign.StartDate;
+                existingCampaign.Goul = campaign.Goul;
+                existingCampaign.Duration = campaign.Duration;
+                _context.SaveChanges();
+            }
+        }
+
+        public void DeleteCampaign(int campaignId)
+        {
+            var campaign = _context.Campaigns.FirstOrDefault(c => c.Id == campaignId);
+            if (campaign != null)
+            {
+                _context.Campaigns.Remove(campaign);
+                _context.SaveChanges();
+            }
+        }
     }
 }
