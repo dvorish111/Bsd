a1d879c7ff97ebd41442a8ff2bb872f541c05878
chana <ch0556766286b@gmail.com>
2023-11-08 11:28:07 +0200

    Add Repositories finish

--- DAL/Repository/DonorRepository.cs
+++ DAL/Repository/DonorRepository.cs
@@ -1,12 +1,58 @@
-﻿using System;
+﻿using DAL.Models;
 using System.Collections.Generic;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace DAL.Repository
 {
-    internal class DonorRepository
+    public class DonorRepository
     {
+        private readonly CampainContext _context;
+
+        public DonorRepository(CampainContext context)
+        {
+            _context = context;
+        }
+
+        public List<Donor> GetAllDonors()
+        {
+            return _context.Donors.ToList();
+        }
+
+        public Donor GetDonorById(int donorId)
+        {
+            return _context.Donors.FirstOrDefault(c => c.Id == donorId);
+        }
+
+        public void AddDonor(Donor donor)
+        {
+            _context.Donors.Add(donor);
+            _context.SaveChanges();
+        }
+
+        public void UpdateDonor(Donor donor)
+        {
+            var existingDonor = _context.Donors.FirstOrDefault(c => c.Id == donor.Id);
+            if (existingDonor != null)
+            {
+                existingDonor.FirstName = donor.FirstName;
+                existingDonor.LastName = donor.LastName;
+                existingDonor.Email = donor.Email;
+                existingDonor.City = donor.City;
+                existingDonor.Phone = donor.Phone;
+                existingDonor.ZipCode = donor.ZipCode;
+                existingDonor.Street = donor.Street;
+                _context.SaveChanges();
+            }
+        }
+
+        public void DeleteDonor(int donorId)
+        {
+            var donor = _context.Donors.FirstOrDefault(c => c.Id == donorId);
+            if (donor != null)
+            {
+                _context.Donors.Remove(donor);
+                _context.SaveChanges();
+            }
+        }
     }
 }
--- DAL/Repository/NeighborhoodRepository.cs
+++ DAL/Repository/NeighborhoodRepository.cs
@@ -1,12 +1,53 @@
-﻿using System;
+﻿using DAL.Models;
 using System.Collections.Generic;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace DAL.Repository
 {
-    internal class NeighborhoodRepository
+    public class NeighborhoodRepository
     {
+        private readonly CampainContext _context;
+
+        public NeighborhoodRepository(CampainContext context)
+        {
+            _context = context;
+        }
+
+        public List<Neighborhood> GetAllNeighborhoods()
+        {
+            return _context.Neighborhoods.ToList();
+        }
+
+        public Neighborhood GetNeighborhoodById(int neighborhoodId)
+        {
+            return _context.Neighborhoods.FirstOrDefault(c => c.Id == neighborhoodId);
+        }
+
+        public void AddNeighborhood(Neighborhood neighborhood)
+        {
+            _context.Neighborhoods.Add(neighborhood);
+            _context.SaveChanges();
+        }
+
+        public void UpdateNeighborhood(Neighborhood neighborhood)
+        {
+            var existingNeighborhood = _context.Neighborhoods.FirstOrDefault(c => c.Id == neighborhood.Id);
+            if (existingNeighborhood != null)
+            {
+                existingNeighborhood.Name = neighborhood.Name;
+               // existingNeighborhood.Donates = neighborhood.Donates;
+                _context.SaveChanges();
+            }
+        }
+
+        public void DeleteNeighborhood(int neighborhoodId)
+        {
+            var neighborhood = _context.Neighborhoods.FirstOrDefault(c => c.Id == neighborhoodId);
+            if (neighborhood != null)
+            {
+                _context.Neighborhoods.Remove(neighborhood);
+                _context.SaveChanges();
+            }
+        }
     }
 }
