2a7d55987bf0447b2aa956c8b260aeaf12a0050e
chanaBarn <149392221+chanaBarn@users.noreply.github.com>
2023-12-17 11:32:30 +0200

    Add mail

--- Campain/Controllers/ContactController.cs
+++ Campain/Controllers/ContactController.cs
@@ -11,38 +11,7 @@ namespace Campain.Controllers
     //[ApiController]
     public class ContactController : BaseController
     {
-        //// GET: api/<ContactController>
-        //[HttpGet]
-        //public IEnumerable<string> Get()
-        //{
-        //    return new string[] { "value1", "value2" };
-        //}
-
-        //// GET api/<ContactController>/5
-        //[HttpGet("{id}")]
-        //public string Get(int id)
-        //{
-        //    return "value";
-        //}
-
-        //// POST api/<ContactController>
-        //[HttpPost]
-        //public void Post([FromBody] string value)
-        //{
-        //}
-
-        //// PUT api/<ContactController>/5
-        //[HttpPut("{id}")]
-        //public void Put(int id, [FromBody] string value)
-        //{
-        //}
-
-        //// DELETE api/<ContactController>/5
-        //[HttpDelete("{id}")]
-        //public void Delete(int id)
-        //{
-        //}
-
+ 
         [HttpPost]
        public IActionResult SendMail(Contact1 contact1)
         {
